9d8a16a096fe6a58742b898b27885f79c018e2b9
이지희 <som2plestory@naver.com>
2024-04-17 17:41:07 +0900

    Add IOException process
    오늘의 운세 스크래핑 IOException 발생시 Retry 최대 3회 추가

--- src/main/java/com/todaysfortune/jsoup/ZodiacFortuneScraper.java
+++ src/main/java/com/todaysfortune/jsoup/ZodiacFortuneScraper.java
@@ -22,37 +22,47 @@ public class ZodiacFortuneScraper {
 	/* StringBuilder에서 List로 변경한 이유
 	 * Text 길이가 길어서 카카오톡으로 공유했을 때 일부 잘려서 보이지 않아서 두 번에 나눠서 보내기 위함 
 	*/
-	public List<String> scrapeZodiacFortune(){
-        
-		try {
-            String url = TodayFortuneConfig.ZODIAC_FORTUNE_URL;
+	public List<String> scrapeZodiacFortune() {
+	    int maxRetries = 3; // 최대 재시도 횟수 설정
+	    int retryCount = 0;
 
-            Document document = Jsoup.connect(url).get();
+	    while (retryCount < maxRetries) {
+	        try {
+	            return performScraping();
+	            
+            // IOException이 발생한 경우, 타임아웃으로 간주하고 재시도
+	        } catch (IOException e) {
+	            retryCount++;
+	            if (retryCount >= maxRetries) {
+	                logger.error("Failed to retrieve fortune information after " + maxRetries + " attempts: " + e.getMessage());
+	                return null;
+	            }
+	            logger.warn("Retrying scraping operation, attempt " + retryCount);
+	        }
+	    }
+	    return null;
+	}
+	
+	private List<String> performScraping() throws IOException{
+		String url = TodayFortuneConfig.ZODIAC_FORTUNE_URL;
+	    Document document = Jsoup.connect(url).get();
 
-            if (document != null) {
-                Element fortuneInfo = document.selectFirst("#card");
-                
-                if (fortuneInfo != null) {
-                    Elements fortuneInfoAll = fortuneInfo.select(".txt_box");
-                    List<String> fortuneList = new ArrayList<>();
-                    for (Element info : fortuneInfoAll) {
-                    	fortuneList.add(info.text() + "\n\n");
-                    }
-                    return fortuneList;
-                    
-                } else {
-                    logger.info("Fortune information not found.");
-                }
-            } else {
-            	logger.info("Failed to retrieve fortune information.");
-            }
-            
-            return null;
-            
-        } catch (IOException e) {
-        	logger.error("An error occurred while scraping the fortune information: " + e.getMessage());
-            e.printStackTrace();
-            return null;
-        }
-    }
+	    if (document != null) {
+	        Element fortuneInfo = document.selectFirst("#card");
+	        if (fortuneInfo != null) {
+	            Elements fortuneInfoAll = fortuneInfo.select(".txt_box");
+	            List<String> fortuneList = new ArrayList<>();
+	            for (Element info : fortuneInfoAll) {
+	                fortuneList.add(info.text() + "\n\n");
+	            }
+	            return fortuneList;
+	        } else {
+	            logger.info("Fortune information not found.");
+	            return null;
+	        }
+	    } else {
+	        logger.info("Failed to retrieve fortune information.");
+	        return null;
+	    }
+	}
 }
